b8499788f990c79779d433fc2f42f59594fc43ee
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-06-18 22:14:09 -0500

    Cambios en el package.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ const cssModules = 'modules&importLoaders=1&localIdentName=[name]__[local]__[has
 
 module.exports = {
     resolve: {
-        extensions: ['', '.js', '.jsx']
-        
+        extensions: ['', '.js', '.jsx']  
     },
 
     entry: ['./src/index.jsx'],
