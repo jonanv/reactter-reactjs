20186794724bc652c46e6fca0c2aab622e37431f
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-04 21:41:36 -0500

    Manejando el estado en el componente App.js

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import 'normalize-css';
 
-import './app.css';
+import styles from './app.css';
 import Header from '../Header';
 import Main from '../Main';
 
 class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            user: {
+                photoURL: 'https://pbs.twimg.com/profile_images/1065088918519988224/EhhCjP4b_400x400.jpg',
+                email: 'jonan-vargas23@hotmail.com',
+                onOpenText: false
+            }
+        }
+    }
+
     render() {
         return (
             <div>
