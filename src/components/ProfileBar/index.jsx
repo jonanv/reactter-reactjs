cddf762af098fbad290eeca34d1647cf06e1652c
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-07 13:08:06 -0500

    Se mando la funcion handleOpenText por medio de los props onOpenText

--- src/components/ProfileBar/index.jsx
+++ src/components/ProfileBar/index.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import styles from './profile-bar.css';
 
 class ProfileBar extends Component {
     render() {
         return (
-            <div>
-
+            <div className={styles.root}>
+                <figure>
+                    <img src={this.props.picture} alt="" className={styles.avatar} />
+                </figure>
+                <span className={styles.username}>Hola @{this.props.username}!</span>
+                <button onClick={this.props.onOpenText} className={styles.button}>
+                    <span className="fa fa-lg fa fa-edit"></span> Tweet!
+                </button>
             </div>
         );
     }
