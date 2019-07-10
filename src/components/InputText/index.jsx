6aedcd8170a0e45e0bdeb78406b3f8e6d3118a07
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-09 21:19:03 -0500

    Se hizo componente InputText y estilos

--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import styles from './input-text.css'
 
 class InputText extends Component {
     render() {
         return (
-            <div>
-                InputText
-            </div>
+            <form className={styles.form} onSubmit={this.props.onSendText}>
+                <textarea className={styles.text} name="text"></textarea>
+                <div className={styles.buttons}>
+                    <button className={styles.close} onClick={this.props.onCloseText}>Cerrar</button>
+                    <button className={styles.send} type="submit">Enviar</button>
+                </div>
+            </form>
         );
     }
 }
