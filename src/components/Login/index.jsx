3f7596a575f75e1c04ab66845de3a936aa3f3bf3
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-16 22:21:10 -0500

    Se creo el componente Login

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './login.css'
+
+class Login extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+}
+
+export default Login;
