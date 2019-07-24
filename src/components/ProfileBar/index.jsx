a2e3c41063e1f0eef0fbee83f59e8910da7e705c
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-23 21:30:31 -0500

    Se hizo propTypes en los componentes faltantes y destructuring

--- src/components/ProfileBar/index.jsx
+++ src/components/ProfileBar/index.jsx
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './profile-bar.css';
 
-class ProfileBar extends Component {
-    render() {
-        return (
-            <div className={styles.root}>
-                <Link to="/profile">
-                    <figure>
-                        <img src={this.props.picture} alt="" className={styles.avatar} />
-                    </figure>
-                </Link>
-                <span className={styles.username}>Hola @{this.props.username}!</span>
-                <button onClick={this.props.onOpenText} className={styles.button}>
-                    <span className="fa fa-lg fa fa-edit"></span> Tweet!
-                </button>
-            </div>
-        );
-    }
+const propTypes = {
+    picture: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    onOpenText: PropTypes.func.isRequired
 }
+
+function ProfileBar({ picture, username, onOpenText }) {
+    return (
+        <div className={styles.root}>
+            <Link to="/profile">
+                <figure>
+                    <img src={picture} alt="" className={styles.avatar} />
+                </figure>
+            </Link>
+            <span className={styles.username}>Hola @{username}!</span>
+            <button onClick={onOpenText} className={styles.button}>
+                <span className="fa fa-lg fa fa-edit"></span> Tweet!
+            </button>
+        </div>
+    );
+}
+
+ProfileBar.propTypes = propTypes;
 
 export default ProfileBar;
