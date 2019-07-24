a0b6035fe4e4b674d317118132dda6a9335a194c
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-23 20:21:50 -0500

    Uso de propiedades con PropTypes en el componente Message

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,7 +1,20 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
-import styles from './message.css'
+import styles from './message.css';
+
+const propTypes = {
+    username: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    displayName: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    date: PropTypes.number.isRequired,
+    numRetweets: PropTypes.number.isRequired,
+    numFavorites: PropTypes.number.isRequired,
+    onReplyTweet: PropTypes.func.isRequired,
+    onRetweet: PropTypes.func.isRequired,
+    onFavorite: PropTypes.func.isRequired,
+}
 
 class Message extends Component {
 
@@ -68,5 +81,7 @@ class Message extends Component {
         );
     }
 }
+
+Message.propTypes = propTypes;
 
 export default Message;
